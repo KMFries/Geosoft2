e5ff834a119164428ad6d809294692aef90c571c
Sven Heitmann <s.s.h@uni-muenster.de>
2013-03-13 11:57:06 +0100

    RunningMidian class modified: Sorting removed because database output is sorted and methods modified to search for median in ArrayLists of ValueSets

--- src/de/wwu/geosoftii/grp5/RunningMedian.java
+++ src/de/wwu/geosoftii/grp5/RunningMedian.java
@@ -1,10 +1,12 @@
 package de.wwu.geosoftii.grp5;
 
+import java.util.ArrayList;
+
 /**
  * This class will calculate the median of a given array. 
  * Furthermore it checks if one or more values are possible outliers.
  * 
- * @author KFries
+ * @author KFries and sven
  * @version 1.0
  *
  */
@@ -18,144 +20,58 @@ public class RunningMedian {
 	 * @param x		the array to be sorted
 	 */
 	
+	private double borderMultiplicator = 1.5;
+	
+	public void setBorderMultiplicator(double borderMultiplicator){
+		this.borderMultiplicator = borderMultiplicator;
+	}
+	
+	
 	public RunningMedian(){
 		
 	}
 	
-	public void sort(double[] x) {
-		qSort(x, 0, x.length-1);
-	}
-	
-	/**
-	 * Quicksort core method.
-	 * 
-	 * @param x			the array to be sorted
-	 * @param left		the number at the first position of the array
-	 * @param right		the number at the last position of the array
-	 */
-	
-	public void qSort(double[] x, int left, int right) {
-		if (left < right) {
-			int i = partition(x, left, right);
-			qSort(x,left,i-1);
-			qSort(x,i+1,right);
-		}
-	}
-	
-	/**
-	 * Partition method of the quicksort algorithm.
-	 * 
-	 * @param x			the array to be sorted
-	 * @param left		the number at the first position of the array
-	 * @param right		the number at the last position of the array
-	 * @return
-	 */
-	
-	public int partition(double[] x, int left, int right) {
-		double pivot = x[right];
-		double help;
-		int i = left;
-		int j = right-1;
-		
-		while(i<=j) {
-			if (x[i] > pivot) {
-				// switch x[i] and x[j]
-				help = x[i];
-				x[i] = x[j];
-				x[j] = help;
-				j--;
-			} else i++;
-		}
-		// switch x[i] and x[right]
-		help = x[i];
-		x[i] = x[right];
-		x[right] = help;
-		
-		return i;
-	}
-	
-	
-	/**
-	 * Method to calculate the median.
-	 * 
-	 * @param Array		the array in which the median shall be calculated
-	 * @return			returns the median of the given array
-	 */
-	
-	 public double Median(double[] Array) {
-		double Median;
-		
-		if (Array.length % 2 == 0)							// even numbers
-			Median = ((Array[Array.length / 2] + Array[(Array.length / 2) - 1]) / 2);
-		else Median = Array[((Array.length + 1) / 2) - 1];	// odd numbers
-		
-		return Median;
-	}
-	
-	 /**
-	  * Method to get the running median without using a window.
-	  * 
-	  * @param Array	the array for which the running median shall be calculated.
-	  */
 	 
-	public void RunningMedian(double[] Array) {	
-		int i = 0;
-		double[] jArray;
-		double RunningMedian;
-		
-		while (i < Array.length) {
-			jArray = new double [i+1];					
-				for	(int m = 0; m <= i; m++)		// initialising help array jArray
-					jArray[m] = Array[m];
-			sort(jArray);							// sort jArray
-			RunningMedian = Median(jArray);
-													
-			System.out.println("Median at step "+(i+1)+": "+RunningMedian);	// give median
-			
-			i++;
-			}
-	}
-	
 	/**
-	 * Method to get the median in a given window and calculate possible outliers.
-	 * 
-	 * @param Array	the array for which the running median and outliers shall be identified.
+	 * Find the Median in a given ArrayList
+	 * @param list is a ArrayList of ValueSets
+	 * @return the median of the given list
 	 */
-	
-	public void RunningWindow(double[] Array) {
-		int windowSize = 4;
-		int i = windowSize;
-		int step = 0;
-		double[] jArray;
-		double RunningMedian;
-		double quartile25, quartile75;
-		double maxBorder, minBorder;
-		double IQR;
+	 public double median(ArrayList<ValueSet> list){
+		 double median;
+		 int size = list.size();
+		 if (list.size() % 2 == 0)							// even numbers
+				median = ( list.get(size / 2 - 1).getValueAsDouble() + list.get(size / 2).getValueAsDouble() ) / 2;
+			else median = list.get(size / 2).getValueAsDouble();	// odd numbers
+		 
+		 return median;
+	 }
+	 
+	/**
+	 * Method checks if a given value is an outlier in comparision to a list of values.
+	 * It calculates the median and checks if it lies in a interval (media +- borderMultiplicator * inter quartile range)
+	 * @param val the value to be checked as ValueSet
+	 * @param list
+	 * @return true if the value is an possible outlier, false if it's not
+	 */
+	public boolean isOutlier(ValueSet val, ArrayList<ValueSet> list){
+		boolean outlierTag = false;
+		//calculate median
+		double median = this.median(list);
+		// calculate 25 % quartile
+		double quartile25 = list.get((int)Math.ceil(list.size()*0.25)-1).getValueAsDouble();
+		// calculate 75 % quartile
+		double quartile75 = list.get((int)Math.ceil(list.size()*0.75)-1).getValueAsDouble();
+		// calculate IQR
+		double IQR = quartile75 - quartile25;
+		// define maxBorder
+		double maxBorder = median + borderMultiplicator * IQR;
+		// define minBorder
+		double minBorder = median - borderMultiplicator * IQR;
+		//check if the tested value is outside of the borders
+		if (val.getValueAsDouble()<minBorder || val.getValueAsDouble()>maxBorder) outlierTag = true;
 		
-		while (i <= Array.length) {
-			jArray = new double[windowSize];
-				for (int m = i - windowSize; m < i; m++)	// initialising help array jArray
-					jArray[m-step] = Array[m];				// we need to subtract the value of step from m to write it to the right position
-			sort(jArray);									// sort jArray
-			RunningMedian = Median(jArray);
-			
-			System.out.println("Median at step "+(step+1)+": "+RunningMedian);
-			
-			quartile25 = jArray[(int) Math.ceil(jArray.length * 0.25) - 1];		// calculate value of the 25% quartile
-			quartile75 = jArray[(int) Math.ceil(jArray.length * 0.75) - 1];		// calculate value of the 75% quartile
-			IQR = quartile75 - quartile25;										// calculate value of the interquartile range
-			maxBorder = RunningMedian + 1.5 * IQR;								// calculate the maximum border for outlier detection
-			minBorder = RunningMedian - 1.5 * IQR;								// calculate the minimum border for outlier detection
-			
-			for (int m = i - windowSize; m < i; m++)
-				if (jArray[m-step] > maxBorder || jArray[m-step] < minBorder)
-					System.out.println("\tPossible Outlier: "+jArray[m-step]);	// check each value inside the window if it could be an outlier
-			
-			step++;
-			i++;
-		}
-			
-		
+		return outlierTag;
 	}
 	
 	
